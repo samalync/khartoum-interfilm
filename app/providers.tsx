cc8f33b74327accaad8269b70e73aeb49ba01bdb
Samalync <support@samalync.com>
2025-12-28 19:18:48 +0200

    7th commit

---
+++ app/providers.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { LanguageProvider } from '../components/LanguageContext';
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <LanguageProvider>{children}</LanguageProvider>;
+}
